adf5c778209ac6cad79466cea20547a043436c34
0802uni <87895769+0802uni@users.noreply.github.com>
2022-01-20 20:48:12 +0900

    プレハブを作らずに配置したオブジェクトでの制作を試みる
    サイコロを振り選択する工程までは仮実装済み

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class Dice : MonoBehaviour
+{
+    [SerializeField]
+    public DiceData diceData;
+    [SerializeField]
+    public bool isRollable;
+    [SerializeField]
+    public bool isSelected;
+
+    DiceManager diceManager;
+
+    Button buttonComponent;
+
+    private void Awake()
+    {
+        diceManager = gameObject.transform.parent.GetComponent<DiceManager>();
+        buttonComponent = gameObject.GetComponent<Button>();
+    }
+
+    private void Start()
+    {
+        diceData = diceManager.datas[0];
+        isRollable = true;
+        isSelected = false;
+
+        buttonComponent.onClick.AddListener(OnClick);
+    }
+
+
+    private void Update()
+    {
+        //サイコロの画像を常に更新、isRollableが正の時に画面をタップ（クリック）で振れる
+        gameObject.GetComponent<Image>().sprite = diceData.pipData;
+
+        if (Input.GetMouseButtonDown(0)&&isRollable)
+        {
+            Roll();
+        }
+    }
+
+    private void Roll()
+    {
+        //ランダムなインデックスを作成し、マネージャーのサイコロの大元の情報配列から引き出す
+        int randomIndex = UnityEngine.Random.Range(0, 6);
+        diceData = diceManager.datas[randomIndex];
+        isRollable = false;
+    }
+
+    private void OnClick()
+    {
+        //ボタンコンポーネントによりマネージャーのDiceClickメソッドが呼び出される
+        diceManager.DiceClick(this);
+    }
+}
---
+++ Assets/Scripts/DiceManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Linq;
+
+public class DiceManager : MonoBehaviour
+{
+    [SerializeField]
+    public List<DiceData> datas;
+
+    [SerializeField]
+    public List<Dice> dices;
+
+    [SerializeField]
+    Button resultButton;
+
+    [SerializeField]
+    GameObject selectedPanel;
+
+    private void Awake()
+    {
+        resultButton.gameObject.SetActive(false);
+        resultButton.onClick.AddListener(Result);
+    }
+
+
+    public void DiceClick(Dice dice)
+    {
+        //クリックしたサイコロと同数値の物が青く表示される
+        //決定ボタンが出現する
+        dices.ForEach(n => n.GetComponent<Image>().color = Color.white);
+        dices.ForEach(n => n.isSelected = false);
+        foreach (var d in dices)
+        {
+            if (d.diceData==dice.diceData)
+            {
+                d.GetComponent<Image>().color = Color.cyan;
+                d.isSelected = true;
+            }
+        }
+        resultButton.gameObject.SetActive(true);
+    }
+
+    private void Result()
+    {
+        //出現したボタンを押すと下部のパネルにサイコロが移動する
+        foreach (var d in dices)
+        {
+            if (d.isSelected)
+            {
+                d.transform.SetParent(selectedPanel.transform);
+                d.GetComponent<Image>().color = Color.white;
+                d.enabled = false;
+            }
+
+            d.isRollable = true;
+        }
+    }
+}
+
+[Serializable]
+public class DiceData
+{
+    public Sprite pipData;
+    public int numData;
+}
